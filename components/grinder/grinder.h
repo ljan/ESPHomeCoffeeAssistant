ab816ee22dde7177316ab843159075fbf758f07d
ljan <schick.ne.email@gmail.com>
2024-10-22 14:08:38 +0200

    Update grinder.h

--- components/grinder/grinder.h
+++ components/grinder/grinder.h
@@ -1,72 +1,57 @@
 #pragma once
-#include "esphome.h"
-#include <TTBOUNCE.h> // Button
 
-enum GrinderState { 
-  GRINDER_IDLE, 
-  GRINDER_SINGLE, 
-  GRINDER_DOUBLE, 
-  GRINDER_MANUAL, 
-  GRINDER_SAVE_SINGLE, 
-  GRINDER_SAVE_DOUBLE 
+#include "esphome.h"
+#include <TTBOUNCE.h>
+
+namespace esphome {
+namespace grinder {
+
+enum GrinderState {
+  GRINDER_IDLE,
+  GRINDER_SINGLE,
+  GRINDER_DOUBLE,
+  GRINDER_MANUAL,
+  GRINDER_SAVE_SINGLE,
+  GRINDER_SAVE_DOUBLE
 };
 
 class GrinderComponent : public Component {
  public:
-  GrinderComponent(int button_pin, int grinder_pin, unsigned long max_grind_time, int single_grind_time, int double_grind_time)
-      : button_pin_(button_pin), grinder_pin_(grinder_pin), max_grind_time_(max_grind_time),
-        single_grind_time_(single_grind_time), double_grind_time_(double_grind_time) {}
+  // Constructor
+  GrinderComponent();
 
+  // Component lifecycle
   void setup() override;
   void loop() override;
   void dump_config() override;
 
-  void set_single_grind_time(int single_grind_time) {
-    this->single_grind_time_ = single_grind_time;
-  }
-  
-  void set_double_grind_time(int double_grind_time) {
-    this->double_grind_time_ = double_grind_time;
-  }
-
-  int get_single_grind_time() const {
-    return this->single_grind_time_;
-  }
-  
-  int get_double_grind_time() const {
-    return this->double_grind_time_;
-  }
+  // Configuration methods
+  void set_button_pin(uint8_t button_pin);
+  void set_grinder_pin(uint8_t grinder_pin);
+  void set_max_grind_time(unsigned long max_grind_time);
+  void set_single_grind_time(int single_grind_time);
+  void set_double_grind_time(int double_grind_time);
 
  protected:
-  static void static_timer_callback(void* p_arg);
-  void single_click();
-  void double_click();
-  void press();
-  void timer_callback();
-
-  int single_grind_time_;
-  int double_grind_time_;
-
- private:
-  int button_pin_;
-  int grinder_pin_;
+  uint8_t button_pin_;
+  uint8_t grinder_pin_;
   unsigned long max_grind_time_;
   int single_grind_time_;
   int double_grind_time_;
-  unsigned long grind_start_time_ = 0;
-  int grind_duration_ = 0;
+  unsigned long grind_start_time_;
+  GrinderState grinder_state_;
 
   TTBOUNCE button_;
   os_timer_t timer_grinder_;
-  GrinderState grinder_state_ = GRINDER_IDLE;
 
-  static const char* TAG;
-
-  // Helper methods for state transitions
-  void handle_single_grind();
-  void handle_double_grind();
-  void handle_manual_grind();
-  void handle_save_single();
-  void handle_save_double();
-  void abort_grind();
+  // Internal methods
+  void start_grinding(int grind_time);
+  void stop_grinding();
+  void timer_callback();
+  void single_click();
+  void double_click();
+  void press();
 };
+
+}  // namespace grinder
+}  // namespace esphome
